ec7f27b3a0f92cb5577a3a413d0e717ab7321be4
wadsaek <wadsaek@gmail.com>
2024-08-29 17:42:26 +0300

    some lsp servers

--- config/lsp/lsp.nix
+++ config/lsp/lsp.nix
@@ -4,8 +4,28 @@
 
     #inlayHints = true; # option doesn't exist?!?!?
     servers = {
-      csharp-ls.enable = true;
+      omnisharp.enable = true;
       nixd.enable = true;
+      clangd.enable = true;
+      fsautocomplete.enable = true;
+      gdscript.enable = true;
+      gopls.enable = true;
+      golangci-lint-ls.enable = true;
+      hls.enable = true;
+      jdt-language-server.enable = true;
+      jsonls.enable = true;
+      yamlls.enable = true;
+      lua-ls.enable = true;
+      nginx-language-server.enable = true;
+      nushell.enable = true;
+      sqls.enable = true;
+      pyright.enable = true;
+      cssls.enable = true;
+      tailwindcss.enable = true;
+      tsserver.enable = true;
+      eslint.enable = true;
+      volar.enable = true;
+      html.enable = true;
     };
 
     keymaps = import ./keymap.nix;
