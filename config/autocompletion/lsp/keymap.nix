772200778777825a4fefabd42be1f7a3040f597c
wadsaek <wadsaek@gmail.com>
2024-09-21 23:41:08 +0300

    added lsp keybinds
    <F10> for renaming
    <leader>co for code actions

--- config/autocompletion/lsp/keymap.nix
+++ config/autocompletion/lsp/keymap.nix
@@ -5,6 +5,8 @@
     gd = "definition";
     gi = "implementation";
     gt = "type_definition";
+    "<F10>" = "rename";
+    "<leader>co" = "code_action";
   };
   diagnostic = {
     "<leader>j" = "goto_next";
