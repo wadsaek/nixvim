fe45f46ad9d3c283206e95d3dc766ab3343c1e56
wadsaek <wadsaek@gmail.com>
2024-09-24 12:18:48 +0300

    Some options

--- config/options.nix
+++ config/options.nix
@@ -2,11 +2,18 @@
   opts ={
     number = true;
     relativenumber = true;
+    
     shiftwidth = 4;
     expandtab = true;
     softtabstop = 4;
+    
     cul = true;
     colorcolumn = "80";
+
+    hlsearch = false;
+    incsearch = true;
+
+    scrolloff =8;
   };
   globals = {
     mapleader = " ";
