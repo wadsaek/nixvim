564cda5072ccf091d70aa9e96d50ad96406f97b6
wadsaek <wadsaek@gmail.com>
2024-10-14 00:28:47 +0300

    added some more plugin specific keymaps

--- config/keymap.nix
+++ config/keymap.nix
@@ -1,13 +1,27 @@
 {
   keymaps = [
     {
-      mode = "n";
       key = "<leader>u";
       action = "<cmd>UndotreeToggle<CR>";
       options = {
         silent = true;
         desc = "Undotree";
       };
+    }
+    {
+      key = "-";
+      action = "<cmd>Oil<CR>";
+      options.desc = "Oil";
+    }
+    {
+      key = "<leader>gs";
+      action = "<cmd>Git<CR>";
+      options.desc = "Open the Fugitive menu";
+    }
+    {
+      key = "<leader>ga";
+      action = "<cmd>Git add %<CR>";
+      options.desc = "Add the current file to git";
     }
   ];
 }
