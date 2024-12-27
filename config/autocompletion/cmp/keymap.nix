be678abf6d15ffa167adda78a7fb85f36daa3f1c
wadsaek <wadsaek@gmail.com>
2024-12-27 12:24:25 +0200

    plugins: changed the cmp confirm bind

--- config/autocompletion/cmp/keymap.nix
+++ config/autocompletion/cmp/keymap.nix
@@ -4,7 +4,7 @@ in
 {
   "<C-Space>" = # lua
     "${cmap}.complete()";
-  "<CR>" = # lua
+  "<C-y>" = # lua
     "${cmap}.confirm({select = true})";
   "<C-j>" = # lua
     "${cmap}.select_next_item(cmp_select)";
