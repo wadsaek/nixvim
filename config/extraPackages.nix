04dd9e0760286d10f9e132c96dfb787d7482d64d
wadsaek <wadsaek@gmail.com>
2025-03-03 16:05:29 +0200

    add dotnet 9 sdk if dotnet is enabled

--- config/extraPackages.nix
+++ config/extraPackages.nix
@@ -1,6 +1,9 @@
-{pkgs,...}:{
-  extraPackages = with pkgs; [
-    nixfmt-rfc-style
-    jq
-  ];
+{pkgs,lib, config,...}:{
+  extraPackages =[
+    pkgs.nixfmt-rfc-style
+    pkgs.jq
+  ] ++ lib.optional config.dotnet.enable (pkgs.dotnetCorePackages.combinePackages [
+    pkgs.dotnetCorePackages.sdk_9_0
+    pkgs.dotnetCorePackages.sdk_8_0
+  ]);
 }
