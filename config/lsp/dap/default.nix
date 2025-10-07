9d025a71a4778b5c0174fb7a75b4885cb4c4bcf7
wadsaek <wadsaek@gmail.com>
2025-10-07 16:40:56 +0300

    disable netcoredbg
    build failure in nixpkgs

--- config/lsp/dap/default.nix
+++ config/lsp/dap/default.nix
@@ -14,25 +14,25 @@
   plugins.dap = {
     enable = true;
     adapters.executables = {
-      coreclr = lib.mkIf config.dotnet.enable {
-        command = "${pkgs.netcoredbg}/bin/netcoredbg";
-        args = [ "--interpreter=vscode" ];
-      };
+      # coreclr = lib.mkIf config.dotnet.enable {
+      #   command = "${pkgs.netcoredbg}/bin/netcoredbg";
+      #   args = [ "--interpreter=vscode" ];
+      # };
     };
     configurations = {
-      cs = lib.mkIf config.dotnet.enable [
-        {
-          type = "coreclr";
-          name = "launch - netcoredbg";
-          request = "launch";
-          program.__raw = # lua
-            ''
-              function()
-                  return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
-              end
-            '';
-        }
-      ];
+      # cs = lib.mkIf config.dotnet.enable [
+      #   {
+      #     type = "coreclr";
+      #     name = "launch - netcoredbg";
+      #     request = "launch";
+      #     program.__raw = # lua
+      #       ''
+      #         function()
+      #             return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+      #         end
+      #       '';
+      #   }
+      # ];
     };
   };
 }
