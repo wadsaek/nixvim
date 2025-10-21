324d560ad08d39eab014ee7cef28df15ceab6ff8
wadsaek <wadsaek@gmail.com>
2025-10-22 00:11:46 +0300

    don't include dotnet sdk

--- config/extraPackages.nix
+++ config/extraPackages.nix
@@ -1,10 +1,7 @@
-{pkgs,lib, config,inputs,system,...}:{
-  extraPackages =[
+{ pkgs, ... }:
+{
+  extraPackages = [
     pkgs.nixfmt-rfc-style
     pkgs.jq
-  ] ++ lib.optional config.dotnet.enable (pkgs.dotnetCorePackages.combinePackages [
-    inputs.nixpkgs-master.legacyPackages.${system}.dotnetCorePackages.sdk_10_0
-    pkgs.dotnetCorePackages.sdk_9_0
-    pkgs.dotnetCorePackages.sdk_8_0
-  ]);
+  ];
 }
